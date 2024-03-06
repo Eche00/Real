0d6ec5306f91903462676697e6c9c748189cb9b0
Eche00 <echeeze956@gmail.com>
2024-03-06 16:59:50 +0100

    i suffer for this bug

--- api/index.js
+++ api/index.js
@@ -25,6 +25,8 @@ app.use("/api/user", userRouter);
 
 app.use("/api/auth", authRouter);
 
+app.use("/api/auth", authRouter);
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal server error";
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,8 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { ArrowBack } from "@mui/icons-material";
 import { Link } from "react-router-dom";
 
 function Signup() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleOnchange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const handleOnsubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+  console.log(formData);
   return (
     <div className=" h-screen flex  items-center justify-center">
       <div className="  flex flex-col p-5 sm:w-[70%] w-[95%] h-[50%] rounded-md">
@@ -11,30 +47,41 @@ function Signup() {
           <ArrowBack />
         </Link>
         <form
+          onSubmit={handleOnsubmit}
           action=""
           className=" flex flex-col items-center justify-center gap-5 h-[100%]">
           <h1 className="text-center text-3xl font-extrabold py-5">Signup</h1>
 
           <input
+            onChange={handleOnchange}
+            id="username"
             type="text"
-            className=" rounded-full border border-blue-500 border-solid p-3 w-[50%] shadow-md shadow-gray-400 outline-none"
+            className=" rounded-full border border-blue-500 border-solid p-3 sm:w-[60%] w-[80%] shadow-md shadow-gray-400 outline-none"
             placeholder="Name"
-            required
           />
           <input
+            onChange={handleOnchange}
+            id="email"
             type="email"
-            className=" rounded-full border border-blue-500 border-solid p-3 w-[80%] shadow-md shadow-gray-400 outline-none"
+            className=" rounded-full border border-blue-500 border-solid p-3 sm:w-[60%] w-[80%] shadow-md shadow-gray-400 outline-none"
             placeholder="Email"
           />
           <input
+            onChange={handleOnchange}
+            id="password"
             type="password"
-            className=" rounded-full border border-blue-500 border-solid p-3 w-[80%] shadow-md shadow-gray-400 outline-none"
+            className=" rounded-full border border-blue-500 border-solid p-3 sm:w-[60%] w-[80%] shadow-md shadow-gray-400 outline-none"
             placeholder="Password"
           />
-          <button className="bg-blue-500 text-2xl text-white py-2 px-20 rounded-full shadow-md shadow-gray-600 my-5 hover:bg-blue-400">
-            Submit
+          <button
+            disabled={loading}
+            className="bg-blue-500 text-2xl text-white py-2 px-20 rounded-full shadow-md shadow-gray-600 my-5 hover:bg-blue-400 disabled:opacity-90 uppercase">
+            {loading ? "Loading..." : "sign up"}
           </button>
           <p className=" gap-5 flex items-center justify-center font-serif text-gray-400">
+            {error && (
+              <p className=" text-sm text-red-500 font-bold">{error}</p>
+            )}
             <span>Already have an account? </span>{" "}
             <Link to="/signin" className=" text-blue-500 hover:underline">
               SignIn
