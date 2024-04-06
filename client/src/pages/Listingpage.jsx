bf3fe9c45e302eec824127ebc935b61bea025651
Eche00 <echeeze956@gmail.com>
2024-04-06 03:30:36 +0100

    uploading image functionality completed

--- client/src/pages/Listingpage.jsx
+++ client/src/pages/Listingpage.jsx
@@ -1,7 +1,77 @@
-import { AddToPhotosOutlined } from "@mui/icons-material";
-import React from "react";
+import {
+  AddToPhotosOutlined,
+  Delete,
+  DeleteOutline,
+} from "@mui/icons-material";
+import React, { useState } from "react";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
 
 function Listingpage() {
+  const [files, setFiles] = useState([]);
+  const [formD, setFormD] = useState({ imageUrls: [] });
+  const [imageUploadError, setImageUploadError] = useState(false);
+  const [uploading, setUploading] = useState(false);
+  console.log(formD);
+
+  const handleImageSubmit = (e) => {
+    if (files.length > 0 && files.length + formD.imageUrls.length < 7) {
+      setUploading(true);
+      setImageUploadError(false);
+      const promises = [];
+      for (let i = 0; i < files.length; i++) {
+        promises.push(storageImage(files[i]));
+      }
+      Promise.all(promises)
+        .then((urls) => {
+          setFormD({ ...formD, imageUrls: formD.imageUrls.concat(urls) });
+          setImageUploadError(false);
+          setUploading(false);
+        })
+        .catch((err) => {
+          setImageUploadError("Image upload failed (2mb max per image)");
+          setUploading(false);
+        });
+    } else {
+      setImageUploadError("You can only upload 6 images per listing");
+      setUploading(false);
+    }
+  };
+  const storageImage = async (file) => {
+    return new Promise((resolve, reject) => {
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(`upload ${progress}% done`);
+        },
+        (error) => {
+          reject(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            resolve(downloadURL);
+          });
+        }
+      );
+    });
+  };
+  const handleRemoveImage = (index) => {
+    setFormD({
+      ...formD,
+      imageUrls: formD.imageUrls.filter((_, i) => i !== index),
+    });
+  };
   return (
     <main className=" max-w-4xl mx-auto sm:p-0 p-5">
       <h1 className=" text-3xl font-semibold text-center my-7">
@@ -123,10 +193,43 @@ function Listingpage() {
             </span>
           </p>
           <div className=" flex items-center border border-blue-500 py-5 px-2 rounded-md my-5">
-            <input type="file" name="" id="images" accept="images/*" multiple />
-            <button className=" w-fit  text-2xl  py-2 px-5 rounded-full shadow-md shadow-gray-600  text-white bg-blue-500 hover:opacity-90 active:opacity-[50%] uppercase self-center object-center ">
-              Upload
+            <input
+              type="file"
+              name=""
+              id="images"
+              accept="images/*"
+              multiple
+              onChange={(e) => setFiles(e.target.files)}
+              className=" w-full"
+            />
+            <button
+              onClick={handleImageSubmit}
+              disabled={uploading}
+              type="button"
+              className=" w-fit  text-2xl  py-2 px-5 rounded-full shadow-md shadow-gray-600  text-white bg-blue-500 hover:opacity-90 active:opacity-[50%] uppercase self-center object-center ">
+              {uploading ? "loading.." : "Upload"}
             </button>
+          </div>
+          <p className=" text-red-700 text-sm font-semibold text-center">
+            {imageUploadError && imageUploadError}
+          </p>
+          <div className=" flex flex-wrap gap-2">
+            {formD.imageUrls.length > 0 &&
+              formD.imageUrls.map((url, index) => (
+                <div key={url} className=" w-fit relative">
+                  <img
+                    src={url}
+                    alt="listing img"
+                    className=" w-20 h-20  object-cover rounded-lg "
+                  />
+                  <button
+                    type="button"
+                    onClick={() => handleRemoveImage(index)}
+                    className=" absolute bottom-0 right-0 text-red-600  cursor-pointer  ">
+                    <Delete />
+                  </button>
+                </div>
+              ))}
           </div>
           <button
             to="/listing"
