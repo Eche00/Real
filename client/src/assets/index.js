c433637dc751592077d7d8eb364d6ab634a745f5
Eche00 <echeeze956@gmail.com>
2024-04-17 12:43:15 +0100

    deve img

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,5 +1,6 @@
 import googlelogo from "./Google.png";
 import heroimg1 from "./hero2.jpeg";
 import heroimg2 from "./hero3.jpeg";
+import dev from "./developer.jpeg";
 
-export { googlelogo, heroimg1, heroimg2 };
+export { googlelogo, heroimg1, heroimg2, dev };
